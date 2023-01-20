f5fdc6e7634466d89e08c507350769863304fbf5
KerkaiwanMam <a370957083131@gmail.com>
2023-01-20 17:32:59 +0700

    HW01 Python1
    HW01 Python1

---
+++ HW01 Python1/test1.py
@@ -0,0 +1,7 @@
+"""
+Chapter : 1 - item : 1 - radius and area of circle
+เขียนโปรแกรม Python ซึ่งรับ input เป็นรัศมีของวงกลม จากนั้นคำนวณพื้นที่และแสดงผล
+    #Testcase : 1
+        r=1.1
+        Area=3.8013271108436504
+"""
---
+++ HW01 Python1/test5.py
@@ -0,0 +1,19 @@
+"""
+Chapter : 1 - item : 5 - vickrey auction
+จงสร้าง vickrey auction แบบจำลอง
+Vickrey auction คือการประมวลที่ผู้ที่จะชนะการประมูล คือ ผู้ที่ยื่นซองเสนอราคาสูงที่สุด แต่จะจ่ายจริงในราคาที่สูงเป็นอันดับสองรองลงมา
+word
+"Enter All Bid : "
+"not enough bidder"
+"error : have more than one highest bid"
+"winner bid is $ need to pay $"
+    Testcase : 1
+        Enter All Bid : 5
+        not enough bidder
+    Testcase : 2
+        Enter All Bid : 5 10 20 5 16
+        winner bid is 20 need to pay 16
+    Testcase : 3
+        Enter All Bid : 5 10 20 20 10
+        error : have more than one highest bid
+"""
